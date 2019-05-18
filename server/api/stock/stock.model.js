5e657090ce6a425e931d37d4a41987845c22e769
shakirullah <shakiru85@gmail.com>
2019-05-18 14:10:31 +0500

    update stock model with new fields

--- server/api/stock/stock.model.js
+++ server/api/stock/stock.model.js
@@ -1,9 +1,16 @@
 import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
 import {registerEvents} from './stock.events';
 
 var StockSchema = new mongoose.Schema({
   name: String,
   quantity: String,
+  gate: String,
+  vehicle: String,
+  Mandi: String,
+  Procured_by : String,
+  grn: String,
+  animals: [{ type: Schema.Types.ObjectId, ref: 'Animals' }]
 });
 
 registerEvents(StockSchema);
