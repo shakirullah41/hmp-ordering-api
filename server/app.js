752a346f074da068b1cc665f22ea2db770a40050
shakirullah <shakiru85@gmail.com>
2019-05-26 13:23:33 +0500

    change

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ registerRoutes(app);
 
 // Start server
 function startServer() {
-    app.angularFullstack = server.listen(config.port, config.ip, function() {
+    app.server = server.listen(config.port, config.ip, function() {
         console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
     });
 }
